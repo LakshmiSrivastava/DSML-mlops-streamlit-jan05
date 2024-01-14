a65dcad419cd350b1f09827500cd892dbcb8b207
LakshmiSrivastava <lakshmi.srivastava20@gmail.com>
2024-01-15 02:01:56 +0530

    changed file

--- car24.py
+++ car24.py
@@ -8,7 +8,7 @@ st.title("Car price prediction app")
 col1, col2 =st.columns(2)
 
 with col1:
-    fuel_type = st.radio('Fuel Type', ['Petrol', 'Diesel', 'CNG', 'Electric'])
+    fuel_type = st.radio('Fuel Type', ['Petrol', 'Diesel', 'LPG', 'Electric'])
     
 with col2:
     transmission_type=st.selectbox('Transmission Type', ['Manual', 'Automatic'])
@@ -20,18 +20,19 @@ with col3:
 with col4:
     seats= st.selectbox('Seats', [2,4,5,6,7,8,9,10])
     
-model = pickle.load(open('car24.py', 're'))
+model = pickle.load(open("car24.ipynb", "rb"))
 
-encode_dict= {
-    'fuel_type': ('Diesel' :1, 'Patrol' : 2, 'CNG':3, 'LPG':4, 'Electric': 5),
-     'transmission_type' :('Manual': 1, 'Automatic':2)}
+encode_dict={
+    'fuel_type': {'Diesel' :1, 'Electric': 2, 'LPG':3,'Petrol' : 4}}
+    #'transmission_type' :{'Manual': 1, 'Automatic':2}}
 
 def model_pred(fuel_type, temperature_type, engine_power, seats):
 
-    transmission_type=encode_dict['transmission_type'][transmission_type]
-    fuel_type=encode_dict['fuel_type'][fuel_type]
+    fuel_type=encode_dict(fuel_type=encode_dict['fuel_type'][fuel_type] )                                # 'transmission_type'][transmission_type]
+
     
-data=[[2018.8, 1, 40000, fuel_type, transmission_type, 18.0, engine_power, 85, seats]]
+    data=[[2018.8, 1, 40000, fuel_type, transmission_type, 18.0, engine_power, 85, seats]]
+    return round(model.predict(data)[0],2)
 
 if st.button('Predict'):
     st.write(model_pred(fuel_type, transmission_type, engine_power, seats))
